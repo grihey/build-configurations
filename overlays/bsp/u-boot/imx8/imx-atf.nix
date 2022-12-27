749d611e6a493dff9375d5a66ea120c4bf636204
Grigoriy Romanov <grigoriy.romanov@unikie.com>
2022-12-27 14:00:55 +0000

    Abstract imx-boot for imx8 boards
    Now is possible to specify for which board imx-boot should
    be created via passing an attribute as a parameter. Hopefully
    rescue directory structure from an undesirable expansion.
    
    Sucessfully tested with both imx8 boards.
    
    Signed-off-by: Grigoriy Romanov <grigoriy.romanov@unikie.com>

--- overlays/bsp/u-boot/imx8/imx-atf.nix
+++ overlays/bsp/u-boot/imx8/imx-atf.nix
@@ -1,18 +1,19 @@
 {
   buildArmTrustedFirmware ,
+  targetBoard ,
 }:
 
 {
-  armTrustedFirmwareiMX8QM = buildArmTrustedFirmware rec {
+  armTrustedFirmwareiMX8 = buildArmTrustedFirmware rec {
     src = fetchGit {
       url = "https://source.codeaurora.org/external/imx/imx-atf";
       ref = "lf_v2.6";
     };
-    platform = "imx8qm";
+    platform = targetBoard;
     enableParallelBuilding = true;
     # To build with tee.bin use extraMakeFlags = [ "bl31 SPD=opteed" ];
     extraMakeFlags = [ "PIE_LDFLAGS=--no-warn-rwx-segments LDFLAGS=--no-warn-rwx-segments" "bl31" ];
     extraMeta.platforms = ["aarch64-linux"];
-    filesToInstall = ["build/${platform}/release/bl31.bin"];
+    filesToInstall = ["build/${targetBoard}/release/bl31.bin"];
   };
 }
